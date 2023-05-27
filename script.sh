6e00c20726db8c85f85ada7ef70df1020776786f
dansocea <dan.socea@gmail.com>
2023-05-27 06:25:17 +0000

    add sh script

---
+++ script.sh
@@ -0,0 +1,22 @@
+# Check if a file is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Error: No file provided."
+    echo "Usage: ./script.sh <file>"
+    exit 1
+fi
+
+file="$1"
+
+# Check if the file exists
+if [ ! -f "$file" ]; then
+    echo "Error: File '$file' does not exist."
+    exit 1
+fi
+
+# Read the file line by line
+while IFS= read -r line; do
+    # Check if the line does not start with #
+    if [[ ! $line =~ ^\# ]]; then
+        echo "$line"
+    fi
+done < "$file"
